41f21298b6f6e4ce8e555d16484a2d78ba0ab6a9
noslenwerdna <nelson.andrew.james@gmail.com>
2015-06-23 21:40:36 +0200

    added check for directory run for submit_LSF.py

--- submit_LSF.py
+++ submit_LSF.py
@@ -11,6 +11,10 @@ process = ("ZnunuB","ZnunuC","ZnunuL","ZeeB","ZeeC","ZeeL",
 processNum = (22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,
               43,44,45,46,47)
 
+if os.getcwd().split("/")[-1]=="CxAODReader":
+    print "Run submit script from top level directory (CxAODFramework)"
+    sys.exit(0)
+
 if not os.access("run_batch_LSF.sh",os.X_OK):
     print "Shell script must be executable. Run:"
     print "chmod 755 run_batch_LSF.sh"
