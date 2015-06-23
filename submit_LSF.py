8399e727e017d6991891827bf412a3af50c4e123
noslenwerdna <nelson.andrew.james@gmail.com>
2015-06-23 21:10:59 +0200

    typo

--- submit_LSF.py
+++ submit_LSF.py
@@ -12,6 +12,6 @@ processNum = (22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,
               43,44,45,46,47)
 
 for p, pn in zip(process,processNum):
-    sp.call(("export process={0}; export processNum={1}; export directory={2} "
+    sp.call(("export process={0}; export processNum={1}; export directory={2}; "
              "bsub -M 16000 -q 1nh -W 450  "
              "run_batch_LSF.sh").format(p,pn,os.getcwd()), shell=True)
